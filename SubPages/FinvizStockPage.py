e3c7e6d736dde0c3a8dbe8d75468d9f90c70c237
kenne <kennethlucia@gmail.com>
2025-11-11 22:13:12 -0500

    Added Page Class for specific Stock.

--- SubPages/FinvizStockPage.py
+++ SubPages/FinvizStockPage.py
@@ -3,8 +3,9 @@ from HomePage.FinvizHomePage import FinvizHomePage
 
 class FinvizStockPage(FinvizHomePage):
 
-    def __init__(self, config_file='config.ini'):
-        super().__init__(config_file)
+    def __init__(self, driver_param):
+        super().__init__(driver_param)
+        self.driver = driver_param
 
     def go_back(self):
         self.driver.back()
