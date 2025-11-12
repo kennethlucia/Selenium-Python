79d0d64e52ace887feb67bf115c8f83157fe8de1
kenne <kennethlucia@gmail.com>
2025-11-11 21:10:37 -0500

    Added method to Search for a Ticker.

--- HomePage/FinvizHomePage.py
+++ HomePage/FinvizHomePage.py
@@ -1,3 +1,7 @@
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
 from BasePage import BasePage
 
 
@@ -5,6 +9,7 @@ from BasePage import BasePage
 class FinvizHomePage(BasePage):
 
     driver = None
+    SEARCH_INPUT = '«r1»'
 
     def __init__(self, driver_param,config_file='config.ini'):
         super().__init__(config_file)
@@ -26,12 +31,21 @@ class FinvizHomePage(BasePage):
     def close_tab(self,tab_number):
         window_handles = self.driver.window_handles
 
+        # point driver at extra tab window handle (Adobe Adblock Installed Tab) and close it.
         self.driver.switch_to.window(window_handles[tab_number])
         print("Tab with Label: " + str("'"+self.driver.title+"'") + " Has Been Closed")
         self.close()
 
+        # get the windows handles again and point driver at the remaining tab handle - there's only
+        # one in the window_handles array. This is the Finviz webpage.
+        window_handles = self.driver.window_handles
+        self.driver.switch_to.window(window_handles[0])
+
     def maximize_window(self):
         self.driver.maximize_window()
+
+    def enter_value(self,value):
+        self.driver.find_element(By.CSS_SELECTOR,'#«r1»').send_keys(value+Keys.ENTER)
 
 
 if __name__ == "__main__":
@@ -39,5 +53,6 @@ if __name__ == "__main__":
     factory.open()
     factory.maximize_window()
     factory.close_tab(1)
+    factory.enter_value("AAPL")
     factory.quit()
 
