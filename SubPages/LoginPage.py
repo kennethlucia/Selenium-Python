a6e0e1cfebfeb94fb14df0e1b12b7791c8ef5fa5
kenne <kennethlucia@gmail.com>
2025-12-09 08:22:42 -0500

    Added extra Html Xpath for choosing Email to Login.

--- SubPages/LoginPage.py
+++ SubPages/LoginPage.py
@@ -13,6 +13,7 @@ class LoginPage(BasePage):
     
     def __init__(self,driver,config_file='config.ini'):
         self.driver = driver
+        self.choose_email = (By.XPATH,'//a[@href="/login-email?remember=true"]')
         self.email = (By.XPATH, '//input[@name="email"]')
         self.password_locator = (By.XPATH, '//input[@name="password"]')
         self.login = (By.XPATH, '//button[@type="submit"]')
@@ -22,6 +23,7 @@ class LoginPage(BasePage):
         user = self.user
         password = self.password
 
+        self.driver.find_element(*self.choose_email).click()
         self.driver.find_element(*self.email).send_keys(user)
         self.driver.find_element(*self.password_locator).send_keys(password)
         self.driver.find_element(*self.login).click()
